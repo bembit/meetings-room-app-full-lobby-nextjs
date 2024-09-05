9e63fb35b16328fe56c63dfc874568b9bd4739f7
bembit <bencemadar89@gmail.com>
2024-09-05 07:51:47 +0200

    some cleaning

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,7 +4,6 @@ export default function Loading() {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-opacity-50"></div>   
-
     </div>
   );
 }
